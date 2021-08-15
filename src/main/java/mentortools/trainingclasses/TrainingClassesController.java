c4f692d7083c79ebae4080a1638c586f639cc736
Radacs-Bence <bence.radacs@gmail.com>
2021-08-15 15:36:32 +0200

    TrainingClassesController -ből fölösleges függőség eltávolítása.

--- src/main/java/mentortools/trainingclasses/TrainingClassesController.java
+++ src/main/java/mentortools/trainingclasses/TrainingClassesController.java
@@ -2,7 +2,6 @@ package mentortools.trainingclasses;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.hibernate.sql.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
